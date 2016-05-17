3223cd6f4303d035f356c26509f7c26bc1a7f95c
MaoEnzhong <maoez@qq.com>
2016-05-17 15:47:00 +0800

    Initial Commit

---
+++ BNRPerson/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BNRPerson
+//
+//  Created by 毛恩忠 on 16/5/17.
+//  Copyright © 2016年 毛恩忠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
